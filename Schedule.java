24e176eab2d6c030fcd37475a124b2517f589b39
Willie Suggs <klbooker09@gmail.com>
2017-05-05 01:47:18 -0500

    Add files via upload

---
+++ Schedule.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class Schedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        String course1, course2, course3, course4, course5, course6, course7, course8, teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7, teacher8;
+       
+        course1 = "JAVA";
+        course2 = "HTML";
+        course3 = "CSS";
+        course4 = "javaScript";
+        course5 = "the meaning of life";
+        course6 = "people";
+        course7 = "reality";
+        course8 = "selfdefense";
+        teacher1 = "Corbin";
+        teacher2 = "me";
+        teacher3 = "myself";
+        teacher4 = "& I";
+        teacher5 = "God";
+        teacher6 = "reality";
+        teacher7 = "neo from the matrix";
+        teacher8 = "bruce lee and goku";
+        
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("| 1 |                    " + course1 + " |             " + teacher1 + " |");
+        System.out.println("| 2 |                    " + course2 + " |                 " + teacher2 + " |"); 
+        System.out.println("| 3 |                     " + course3 + " |             " + teacher3 + " |");
+        System.out.println("| 4 |              " + course4 + " |                " + teacher4 + " |");
+        System.out.println("| 5 |     " + course5 + " |                " + teacher5 + " |");
+        System.out.println("| 6 |                  " + course6 + " |            " + teacher6 + " |");
+        System.out.println("| 7 |                 " + course7 + " |" + teacher7 + " |");
+        System.out.println("| 8 |             " + course8 + " | " + teacher8 + " |");
+        System.out.println("+--------------------------------------------------+");
+       
+    }
+    
+}
